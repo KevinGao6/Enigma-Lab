382748a8948ce1a2df3b0e513565d4aa1a217b16
eomuratoglu <33694486+eomuratoglu@users.noreply.github.com>
2018-04-04 11:28:46 -0400

    public methods

---
+++ src/Enigma.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ * @author ezramuratoglu, Kevin Gao
+ *
+ */
+public class Enigma extends Cryptogram 
+{
+	public Enigma(int n)
+	{
+		
+	}
+	
+	/**
+	 * sets the i-th element of the look-up table to ch
+	 * @param i
+	 * @param ch
+	 */
+	public void setSubstitution(int i, char ch)
+	{
+		
+	}
+	
+	/**
+	 * decodes all the letters in text according to the current lookup table. Leaces all characters
+	 * that are not letters unchanged and preserves the upper/lower case of letters. Returns the 
+	 * decoded String which should be the same length as text.
+	 * @param text
+	 * @return
+	 */
+	public String decode(String text)
+	{
+		
+	}
+	
+	/**
+	 * returns computer-generated hints for each letter in the encrypted text.
+	 * @param text
+	 * @param lettersByFrequency
+	 */
+	public getHints(String text, String lettersByFrequency)
+	{
+		
+	}
+	
+	
+}
