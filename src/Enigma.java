1a9a19aa4815047a5e3b684c0495b9063fe02a26
eomuratoglu <33694486+eomuratoglu@users.noreply.github.com>
2018-04-06 08:36:03 -0400

    tentative decode method

--- src/Enigma.java
+++ src/Enigma.java
@@ -7,9 +7,13 @@ public class Enigma extends Cryptogram
 {
 	private char[] lookupTable;
 	
+	/**
+	 * constructor
+	 * @param n should be the number of letters in the alphabet
+	 */
 	public Enigma(int n)
 	{
-		
+		lookupTable = new char[n];
 	}
 	
 	/**
@@ -23,7 +27,7 @@ public class Enigma extends Cryptogram
 	}
 	
 	/**
-	 * decodes all the letters in text according to the current lookup table. Leaces all characters
+	 * decodes all the letters in text according to the current lookup table. Leaves all characters
 	 * that are not letters unchanged and preserves the upper/lower case of letters. Returns the 
 	 * decoded String which should be the same length as text.
 	 * @param text
@@ -31,7 +35,29 @@ public class Enigma extends Cryptogram
 	 */
 	public String decode(String text)
 	{
-		
+		boolean isUpperCase;
+		String result = "";
+		for(int i = 0; i < text.length(); i++)
+		{
+			char character = text.charAt(i);
+			isUpperCase = Character.isUpperCase(character);
+			int index = -1;
+			
+			//find index in lookupTable
+			for(int x = 0; x < lookupTable.length; x++)
+				if(lookupTable[x] == character)
+					index = x;
+
+			if(index != -1)
+				if(isUpperCase)
+					result += Character.toUpperCase(lookupTable[index]);
+				else 
+					result += Character.toLowerCase(lookupTable[index]);
+			else
+				result += character;
+			
+		}
+		return result;
 	}
 	
 	/**
