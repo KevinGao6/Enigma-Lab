1b0ef81215542e4481abc2f767f3ef38042cbb15
KevinGao6 <32113541+KevinGao6@users.noreply.github.com>
2018-04-10 12:17:19 -0400

    countletters

--- src/Enigma.java
+++ src/Enigma.java
@@ -23,7 +23,7 @@ public class Enigma extends Cryptogram
 	}
 	
 	/**
-	 * decodes all the letters in text according to the current lookup table. Leaces all characters
+	 * decodes all the letters in text according to the current lookup table. Leaves all characters
 	 * that are not letters unchanged and preserves the upper/lower case of letters. Returns the 
 	 * decoded String which should be the same length as text.
 	 * @param text
@@ -35,18 +35,34 @@ public class Enigma extends Cryptogram
 	}
 	
 	/**
-	 * returns computer-generated hints for each letter in the encrypted text.
+	 * Returns computer-generated hints for each letter in the encrypted text.
 	 * @param text
 	 * @param lettersByFrequency
 	 */
 	public String getHints(String text, String lettersByFrequency)
 	{
+		int[] frequencies = countLetters(text);
+		String result = "";
+		
 		
 	}
 	
+	/**
+	 * Private helper method that counts the number of occurrences for each of the letters a - z and saves these counts in an array
+	 * @param text
+	 * @return
+	 */
 	private int[] countLetters(String text)
 	{
+		int[] counts = new int[26];
 		
+		for(int i = 0; i < text.length(); i++)
+		{
+			int j = (int)(text.charAt(i)) - 65;
+			counts[j] = counts[j] ++;
+		}
+		
+		return counts;
 	}
 	
 	
